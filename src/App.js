570e191593976c251593a8b343b4bdf13addcd24
KapilKumar18 <83170066+KapilKumar18@users.noreply.github.com>
2021-07-29 19:32:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ import EditContact from './component/EditContact';
 const App = () => {
 
   const [ contacts, setContacts ] = useState([]);
+  const [searchItem, setSearchItem] = useState("");
+  const [ searchContacts, setSearchContacts ] = useState([]);
 
   const addContact = (contact) => {
-    setContacts([{ id: uuid() , ...contact }, ...contacts])
+    setContacts([...contacts, { id: uuid() , ...contact }])
   }
 
   const deleteContact = (id) => {
@@ -30,6 +32,19 @@ const App = () => {
     })
   )}
 
+  const searchContact = (searchvalue) => {
+    setSearchItem(searchvalue)
+    if(searchvalue !== ""){
+       const newSearchList = contacts.filter((contact) => {
+        return Object.values(contact).join(" ").includes(searchvalue);
+     })
+     setSearchContacts(newSearchList)
+    }
+    else{
+      setSearchContacts(contacts)
+    }
+  }
+
   useEffect(()=>{
     const getContact = JSON.parse(localStorage.getItem("contacts"))
     getContact && setContacts(getContact)
@@ -45,7 +60,10 @@ const App = () => {
         <Router>
         <Switch>
           <Route exact path="/" render={(props) =>
-            <ContactList {...props} contacts={contacts} deleteContactHandler={deleteContact} /> 
+            <ContactList {...props} contacts={ searchItem.length < 1 ? contacts : searchContacts } deleteContactHandler={deleteContact} 
+            searchItem={searchItem}
+            searchContactHandler={ searchContact } 
+            /> 
           }></Route>
 
           <Route exact path="/addcontact" render={ (props) =>  
